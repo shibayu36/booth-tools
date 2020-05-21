07f793f99a1900c341b01f7a02e582b23efa87a1
shibayu36 <shibayu36@gmail.com>
2020-05-21 11:42:31 +0900

    format

--- get-booth-orders.js
+++ get-booth-orders.js
@@ -1,8 +1,8 @@
 // Usage
 // PIXIV_ID=... PIXIV_PASSWORD=... node get-booth-orders.js
 
-const puppeteer = require('puppeteer-extra');
-const fs = require('fs');
+const puppeteer = require("puppeteer-extra");
+const fs = require("fs");
 
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(pluginStealth());
@@ -13,7 +13,9 @@ async function getOrders(browser, page) {
 
   // 商品リンクを抜き出す
   let orderLinks = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll('a.link-ext')).map(a => a.href);
+    return Array.from(document.querySelectorAll("a.link-ext")).map(
+      (a) => a.href
+    );
   });
 
   // それぞれの商品詳細から名前、商品リストを抜き出す
@@ -21,10 +23,17 @@ async function getOrders(browser, page) {
   for (let link of orderLinks) {
     await orderPage.goto(link);
     let row = await orderPage.evaluate(() => {
-      let panel = document.querySelectorAll('.manage-panel')[0];
-      let name = panel.querySelector('.manage-order-shipment-summaries .u-tpg-slight-body').textContent.match(/(.+)\s+様/)[1];
-      let productContainers = panel.querySelectorAll('.manage-order-content');
-      let products = Array.from(productContainers).map(c => c.querySelector('.u-tpg-slight-body a.nav-reverse').innerText.trim() + " : " + c.querySelector('.u-text-right b').innerText);
+      let panel = document.querySelectorAll(".manage-panel")[0];
+      let name = panel
+        .querySelector(".manage-order-shipment-summaries .u-tpg-slight-body")
+        .textContent.match(/(.+)\s+様/)[1];
+      let productContainers = panel.querySelectorAll(".manage-order-content");
+      let products = Array.from(productContainers).map(
+        (c) =>
+          c.querySelector(".u-tpg-slight-body a.nav-reverse").innerText.trim() +
+          " : " +
+          c.querySelector(".u-text-right b").innerText
+      );
       return name + "\t" + products.sort().reverse().join("\t");
     });
     console.log(row);
@@ -36,24 +45,30 @@ async function getOrders(browser, page) {
 }
 
 (async () => {
-
   const browser = await puppeteer.launch({
     devtools: !!process.env["DEBUG"],
-    slowMo: 50
+    slowMo: 50,
   });
   let page = await browser.newPage();
 
   // loginする
-  await page.goto('https://accounts.pixiv.net/login?view_type=popup&source=booth&return_to=https%3A%2F%2Faccounts.pixiv.net%2Fpopup.html%23https%3A%2F%2Fbooth.pm%2Fusers%2Fsign_in');
-  await page.type('#LoginComponent input[autocomplete="username"]', process.env.PIXIV_ID);
-  await page.type('#LoginComponent input[autocomplete="current-password"]', process.env.PIXIV_PASSWORD);
+  await page.goto(
+    "https://accounts.pixiv.net/login?view_type=popup&source=booth&return_to=https%3A%2F%2Faccounts.pixiv.net%2Fpopup.html%23https%3A%2F%2Fbooth.pm%2Fusers%2Fsign_in"
+  );
+  await page.type(
+    '#LoginComponent input[autocomplete="username"]',
+    process.env.PIXIV_ID
+  );
+  await page.type(
+    '#LoginComponent input[autocomplete="current-password"]',
+    process.env.PIXIV_PASSWORD
+  );
   await page.click('#LoginComponent button[type="submit"]');
-  await page.goto('https://booth.pm/users/sign_in');
+  await page.goto("https://booth.pm/users/sign_in");
   console.log("finish to login");
 
-
   // 未発送ページへ
-  await page.goto('https://manage.booth.pm/orders?state=paid');
+  await page.goto("https://manage.booth.pm/orders?state=paid");
 
   // 未発送ページを全部たどって購入状況を全てたどる
   let results = [];
@@ -65,16 +80,18 @@ async function getOrders(browser, page) {
     let nextLink = await page.$('a[rel="next"]');
     if (nextLink) {
       nextLink.click();
-      await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"});
-    }
-    else {
+      await page.waitForNavigation({
+        timeout: 60000,
+        waitUntil: "domcontentloaded",
+      });
+    } else {
       break;
     }
   }
 
-  fs.writeFileSync('result.tsv', results.join("\n"));
+  fs.writeFileSync("result.tsv", results.join("\n"));
 
   browser.close();
 
-  console.log('done');
+  console.log("done");
 })();
