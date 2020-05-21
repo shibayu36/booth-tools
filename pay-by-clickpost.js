9b759f25f15808f71b9e22b9507c255026f9df6e
shibayu36 <shibayu36@gmail.com>
2020-05-21 12:36:42 +0900

    format

--- pay-by-clickpost.js
+++ pay-by-clickpost.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 const YAHOO_ID = process.env["YAHOO_ID"];
 const YAHOO_PASSWORD = process.env["YAHOO_PASSWORD"];
@@ -11,13 +11,15 @@ if (!(YAHOO_ID && YAHOO_PASSWORD)) {
 // 支払い待ちから一件支払う。
 // 支払い待ちがなければ0、あれば1を返す
 async function payOnce(page) {
-  await page.goto('https://clickpost.jp/mypage/index');
+  await page.goto("https://clickpost.jp/mypage/index");
   await page.waitForSelector('input[value="一時保存"]');
   let listLink = await page.$('input[value="一時保存"]');
   listLink.click();
 
   await page.waitForNavigation({ waitUntil: "networkidle0" });
-  let firstPayment = await page.$('.home_contents table tbody input[type="submit"]');
+  let firstPayment = await page.$(
+    '.home_contents table tbody input[type="submit"]'
+  );
   if (!firstPayment) {
     return 0;
   }
@@ -25,13 +27,13 @@ async function payOnce(page) {
   firstPayment.click();
   await page.waitForNavigation();
 
-  let cvcInput = await page.$('#PaymentDispFormCVC');
+  let cvcInput = await page.$("#PaymentDispFormCVC");
   if (cvcInput) {
-    page.type('#PaymentDispFormCVC', CARD_CVC);
+    page.type("#PaymentDispFormCVC", CARD_CVC);
     await page.waitFor(1000);
   }
 
-  let paySubmit = await page.$('input#CompleteBtn');
+  let paySubmit = await page.$("input#CompleteBtn");
   paySubmit.click();
   await page.waitForNavigation();
 
@@ -45,30 +47,35 @@ async function payOnce(page) {
 (async () => {
   const browser = await puppeteer.launch({
     devtools: !!process.env["DEBUG"],
-    slowMo: 50
+    slowMo: 50,
   });
 
   const page = await browser.newPage();
 
   // yahoo login
   {
-    await page.goto('https://login.yahoo.co.jp/config/login', { waitUntil: "networkidle0" });
+    await page.goto("https://login.yahoo.co.jp/config/login", {
+      waitUntil: "networkidle0",
+    });
     await page.type('input[name="login"]', YAHOO_ID);
-    let nextLoginLink = await page.$('#btnNext');
+    let nextLoginLink = await page.$("#btnNext");
     nextLoginLink.click();
 
     await page.waitForSelector('input[name="passwd"]', { visible: true });
     await page.type('input[name="passwd"]', YAHOO_PASSWORD);
-    let loginLink = await page.$('#btnSubmit');
+    let loginLink = await page.$("#btnSubmit");
     loginLink.click();
-    await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"});
+    await page.waitForNavigation({
+      timeout: 60000,
+      waitUntil: "domcontentloaded",
+    });
   }
 
-  console.log('yahoo login finished');
+  console.log("yahoo login finished");
 
   // clickpost login
   {
-    await page.goto('https://clickpost.jp/mypage/index');
+    await page.goto("https://clickpost.jp/mypage/index");
 
     let yLoginButton = await page.$('.logInBtn a[href="/auth/yconnect"]');
     yLoginButton.click();
@@ -78,9 +85,9 @@ async function payOnce(page) {
     submitButton.click();
     await page.waitForNavigation();
   }
-  console.log('clickpost login finished');
+  console.log("clickpost login finished");
 
-  console.log('payment start');
+  console.log("payment start");
 
   while (true) {
     let paymentExists = await payOnce(page);
@@ -90,8 +97,7 @@ async function payOnce(page) {
     // 素早く決済しすぎないように待つ
     await page.waitFor(5000);
   }
-  console.log('payment finish');
+  console.log("payment finish");
 
   browser.close();
-
 })();
